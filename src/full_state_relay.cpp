fdab9b4d827a016c35068c20bcb03dfd2f1d7f3c
evannsm <evannsmcuadrado@gmail.com>
2025-09-02 23:05:59 -0400

    update fullstaterelay to use new minimal px4_msgs version of the .msg def

--- src/full_state_relay.cpp
+++ src/full_state_relay.cpp
@@ -2,7 +2,8 @@
 
 #include <px4_msgs/msg/vehicle_odometry.hpp>
 #include <px4_msgs/msg/vehicle_local_position.hpp>
-#include <mocap4r2_msgs/msg/full_state.hpp>
+#include <px4_msgs/msg/full_state.hpp>
+// #include <mocap4r2_msgs/msg/full_state.hpp>
 
 #include <chrono>
 #include <functional>
@@ -34,7 +35,7 @@ public:
             "/fmu/out/vehicle_local_position", px4_qos_sub,
             std::bind(&FullStateRelay::localPositionCallback, this, _1));
 
-        full_state_pub_ = create_publisher<mocap4r2_msgs::msg::FullState>(
+        full_state_pub_ = create_publisher<px4_msgs::msg::FullState>(
             "/merge_odom_localpos/full_state_relay", px4_qos_pub);
 
         timer_pub_full_state_ = this->create_wall_timer(
@@ -45,7 +46,7 @@ private:
     // --- Subscriptions / publications ---
     rclcpp::Subscription<px4_msgs::msg::VehicleOdometry>::SharedPtr vehicle_odometry_sub_;
     rclcpp::Subscription<px4_msgs::msg::VehicleLocalPosition>::SharedPtr local_position_sub_;
-    rclcpp::Publisher<mocap4r2_msgs::msg::FullState>::SharedPtr full_state_pub_;
+    rclcpp::Publisher<px4_msgs::msg::FullState>::SharedPtr full_state_pub_;
     rclcpp::TimerBase::SharedPtr timer_pub_full_state_;
 
     // --- Messages ---
@@ -140,7 +141,7 @@ private:
     }
 
     // --- State & gating params ---
-    mocap4r2_msgs::msg::FullState full_state_msg_{};
+    px4_msgs::msg::FullState full_state_msg_{};
 
     rclcpp::Time last_vo_cb_time_{0, 0, RCL_ROS_TIME};
     rclcpp::Time last_lp_cb_time_{0, 0, RCL_ROS_TIME};
